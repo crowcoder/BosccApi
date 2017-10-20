1a1b302c23759a16e38def8e07343ddcf19fea01
Bob Crowley <bob.crowley@evopayments.com>
2017-10-20 14:17:38 -0400

    checking in before push to Github

--- BosccApi/Data/PostsRepository.cs
+++ BosccApi/Data/PostsRepository.cs
@@ -11,24 +11,11 @@ namespace BosccApi.Data
     public class PostsContext : DbContext
     {
         public PostsContext(DbContextOptions<PostsContext> options)
-            :base(options)
+            : base(options)
         {
 
         }
 
         public DbSet<Post> Posts { get; set; }
-    }
-
-    public class PostsRepository
-    {
-
-
-        private readonly string _dbPath = "~/Data/posts.sqlite";
-        
-
-        public Task<List<BosccApi.Models.Post>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
